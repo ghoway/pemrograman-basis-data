5debffa258add2ffcca6e1e5e4557b5d660db944
Wahyu Hidayatullah <ghoway@gmail.com>
2025-10-24 09:39:48 +0700

    add routes on nav app name

--- UTS/411241061_wahyu_hidayatullah/resources/views/partials/navbar.blade.php
+++ UTS/411241061_wahyu_hidayatullah/resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-blue-600 p-4">
     <div class="container mx-auto flex justify-between items-center">
-        <div class="text-white font-bold text-xl">{{ config('app.name') }}</div>
+        <a href="{{ url('/') }}" class="text-white font-bold text-xl">{{ config('app.name') }}</a>
         <div class="space-x-4">
             <a href="{{ route('dashboard') }}" class="text-white hover:text-gray-200">Dashboard</a>
             <a href="{{ route('pelanggan.index') }}" class="text-white hover:text-gray-200">Pelanggan</a>
